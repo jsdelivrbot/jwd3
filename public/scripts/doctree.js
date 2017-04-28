22ac9923bd2983857db72a4a505dce9c85e42dd4
newhope <jwinterday@mail.ru>
2017-04-28 11:12:53 +0300

    +fixed pagenum

--- public/scripts/doctree.js
+++ public/scripts/doctree.js
@@ -293,4 +293,30 @@ $(document).ready(function () {
         });
         return false;
     });
+
+
+
+    var h_hght = 630; // высота шапки
+    var h_mrg = 10;// отступ когда шапка уже не видна
+                 
+    $(function(){
+ 
+        var elem = $('#pdfPage');
+        var top = $(this).scrollTop();
+     
+        if(top > h_hght){
+            elem.css('top', h_mrg);
+        }           
+     
+        $(window).scroll(function(){
+            top = $(this).scrollTop();
+         
+            if (top+h_mrg < h_hght) {
+                elem.css('top', (h_hght-top));
+            } else {
+                elem.css('top', h_mrg);
+            }
+        });
+ 
+    });
 });
